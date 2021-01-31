580984fa519a98403f8874c5f4b44582199008f3
Dzenit110 <72405540+Dzenit110@users.noreply.github.com>
2021-01-31 16:44:53 +0300

    Perfect number
    A perfect number is a number whose sum of all positive divisors except itself is equal to itself. Ask the user to enter a number. Then, code the code to find out whether the entered number is perfect or not with c #. As a screen output, print the divisors of the number and the number on the screen.

---
+++ Perfect number/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _191307095DzenitHafta12Odev10
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            
+            bool kontrol = true;
+            
+            int girissayi;
+            do
+            {
+                Console.WriteLine("Lutfen sayinizi giriniz:");
+
+
+                if ((kontrol = int.TryParse(Console.ReadLine(), out girissayi)) == false)
+                {
+                    Console.WriteLine("Yanlış giriş ");
+                }
+
+
+            } while (kontrol==false);
+            
+
+            int sayi = 0;
+
+            
+            for (int i = 1; i < girissayi; i++)
+            {
+                if (girissayi % i == 0)
+                {
+                    sayi += i;
+
+                    Console.WriteLine("Sayının Bölenlerini:" + i);
+
+                    Console.WriteLine();
+                    
+                }
+            }
+
+            
+
+            
+
+            Console.WriteLine();
+
+            if (sayi == girissayi)
+            {
+                Console.WriteLine(girissayi + " sayı mükemmel sayıdır");
+            }
+
+
+            else
+            {
+                Console.WriteLine(girissayi + " sayı mükemmel sayı değildir");
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
