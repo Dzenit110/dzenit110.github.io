c8ff94aaa158a032730b91db7d33f5057732db8f
Dzenit110 <72405540+Dzenit110@users.noreply.github.com>
2021-01-31 16:48:30 +0300

    Triangular numbers
    Numbers that give the sum of consecutive natural numbers from 1 to n are called Triangular numbers. These are numbers that can be written as (n * (n + 1)) / 2, where n is the count.
    
    For example: 15 is a triangular number. The sum of counts up to 5: 1 + 2 + 3 + 4 + 5 = 15

---
+++ Triangular numbers/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _191307095DzenitHafta13Odev11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool kontrol = true;
+
+            int girilenSayi;
+                Yanlis:
+            do
+            {
+                Console.WriteLine("Lütfen  sayınızı giriniz:");
+
+            
+                if ((kontrol = int.TryParse(Console.ReadLine(), out girilenSayi)) == false)
+                {
+                    
+
+                    Console.WriteLine("Yanlış giriş!");
+                }
+
+
+            } while (kontrol == false);
+
+
+            int girilenSayideger = girilenSayi;
+            int eldekiBasamak = 0;
+            int toplam = 0;
+            int sayi = 1;
+
+
+            while (girilenSayi > 0)
+            {
+                girilenSayi = girilenSayi / 10;
+                eldekiBasamak = eldekiBasamak + 1;
+            }
+
+            if (eldekiBasamak <= 4 && girilenSayideger > 0)
+            {
+
+
+
+                while (toplam < girilenSayideger)
+                {
+                    toplam= toplam + sayi;
+
+                    sayi = sayi + 1;
+
+                }
+                     
+
+                if (toplam == girilenSayideger)
+                {
+                    Console.WriteLine(girilenSayideger+" üçgen sayıdır");
+                }
+
+                else
+                {
+                    Console.WriteLine(girilenSayideger + " üçgen sayı değildir");
+                }
+                        
+                
+            }
+
+            else
+            {
+                Console.WriteLine("Yanlış giriş, lütfen en fazla 4 basamaklı ve pozitif bir sayı olması gerekiyor.");
+                goto Yanlis;
+                
+            }
+
+
+                Console.ReadKey();
+
+        }
+    }
+}
