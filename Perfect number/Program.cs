3712c585a9f5816b1004b9a7a83d88712e68ac87
Dzenit110 <72405540+Dzenit110@users.noreply.github.com>
2021-02-21 20:12:13 +0300

    Perfect number
    Perfect number

---
+++ Perfect number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama
+{
+    
+    class Program
+    {
+        static int j = 100;
+       static double i = 1;
+
+        static double Topla(double i)
+        {
+            i = 2;
+
+            return i ;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            double i = 3;
+
+            Console.WriteLine("i değeri:" + i);
+            Console.WriteLine("i değeri:" + (Program.i));
+            Console.WriteLine("i değeri:" + Topla(i));
+            Console.WriteLine("j değeri:" + Program.j);
+
+            Console.ReadKey();
+        }
+        
+       
+    }
+}
