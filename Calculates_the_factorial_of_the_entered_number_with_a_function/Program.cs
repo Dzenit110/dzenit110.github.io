1868e368c382bb188d48e29fc4754cf3838d7f43
Dzenit110 <72405540+Dzenit110@users.noreply.github.com>
2021-09-06 15:49:25 +0200

    Code_C#

---
+++ Calculates_the_factorial_of_the_entered_number_with_a_function/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Örnek_3
+{
+    class Program
+    {    /* Girilen sayının faktöriyelini faktHayır isimli bir fonksiyon ile hesaplayan 
+            ve hesapladığı değeri ana programa geri döndüren ve 
+            yazdıran algoritma . (Örn:5 için; 5*4*3*2*1= 120)*/
+
+
+        static int fakt(int sayi)
+        {
+            
+            int degerSayi = sayi;
+            int toplam = 1;
+
+            for (int i = 1; i <= degerSayi; i++)
+            {
+                Console.Write(i + "*" + toplam);
+                toplam = toplam * i;
+            }
+
+
+            sayi = toplam;
+            return sayi;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int girilenSayi;
+
+            Console.WriteLine("Lütfen bir sayınızı giriniz:");
+
+            girilenSayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("=" + fakt(girilenSayi));
+           // Console.WriteLine("Sayının faktöriyeli:" + fakt(girilenSayi));   bu kismi iki kez yazmak bak !!!
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ Positive_and_negative_numbers_up_to_10/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Örnek_2
+{         //Girilen 10 adet sayı içerisinden pozitiflerin ve negatiflerin sayısını bulan ve yazdıran akış şeması.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] d1 = new int[10];
+
+            Console.WriteLine("Lütfen 10 adet sayınızı yazınız:");
+
+
+            for (int i = 0; i < d1.Length; i++)
+            {
+
+                d1[i] = Convert.ToInt32(Console.ReadLine());   //  Burada yazarken "1 2 3.." olmasi gerekiyor
+                                                               // Ayrıca n sayıları girerse 10 adet değil !!
+                
+            }
+
+
+            Console.Write("Pozitif sayıları:");
+            for (int i = 0; i < d1.Length; i++)
+            {
+                if (d1[i] > 0)
+                {
+
+                    Console.Write(" " + d1[i]);
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Negatif sayıları:");
+
+                for (int n = 0; n < d1.Length; n++)
+                {
+                    if (d1[n]<0)
+                    {
+                        
+                        Console.Write(" "+d1[n]);
+                    }
+                }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
